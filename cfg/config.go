a455f6b79a9e5ea88ed73a8320f339224f9243b9
Thomas von Dein <tom@vondein.org>
2025-01-30 17:31:56 +0100

    bump version

--- cfg/config.go
+++ cfg/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 const DefaultSeparator string = `(\s\s+|\t)`
-const Version string = "v1.3.1"
+const Version string = "v1.3.2"
 const MAXPARTS = 2
 
 var DefaultConfigfile = os.Getenv("HOME") + "/.config/tablizer/config"
