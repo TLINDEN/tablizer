fe01fa26bbbc95c83f220ffc1fd6dc817fb399bd
Thomas von Dein <tom@vondein.org>
2025-09-09 21:59:08 +0200

    added styled help buffer

--- cfg/config.go
+++ cfg/config.go
@@ -28,7 +28,7 @@ import (
 )
 
 const DefaultSeparator string = `(\s\s+|\t)`
-const Version string = "v1.5.0"
+const Version string = "v1.5.1"
 const MAXPARTS = 2
 
 var DefaultConfigfile = os.Getenv("HOME") + "/.config/tablizer/config"
