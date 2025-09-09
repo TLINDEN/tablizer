58e2f7ae0cd7ff71e47a8ced7c9f09327fa7da91
Thomas von Dein <tom@vondein.org>
2025-09-09 19:56:57 +0200

    finally fixed resizing isse, added comments for my future self

--- lib/tableeditor.go
+++ lib/tableeditor.go
@@ -28,7 +28,7 @@ import (
 	"github.com/tlinden/tablizer/cfg"
 )
 
-// This one exists outside of the bubble loop, and is referred to as
+// The context exists outside of the bubble loop, and is being used as
 // pointer reciever. That way we can use it as our primary storage
 // container.
 type Context struct {
@@ -46,6 +46,9 @@ type Context struct {
 	verticalMargin   int
 }
 
+// Execute tablizer sort function, feed it with fresh config, we do
+// NOT use the existing runtime config, because sorting is
+// configurable in the UI separately.
 func (ctx *Context) Sort(mode string) {
 	conf := cfg.Config{
 		SortMode:        mode,
@@ -58,6 +61,8 @@ func (ctx *Context) Sort(mode string) {
 	sortTable(conf, ctx.data)
 }
 
+// The actual table model, holds the context pointer, a copy of the
+// pre-processed data and some flags
 type FilterTable struct {
 	Table table.Model
 
@@ -78,10 +83,12 @@ const (
 	// header+footer
 	ExtraRows = 5
 
-	HELP = "?:help | "
+	HelpFooter = "?:help | "
 
+	// number of lines taken by help below, adjust accordingly!
 	HelpRows = 7
 
+	// shown when the user presses ?
 	LongHelp = `
 Key bindings usable in interactive filter table:
 q         commit and quit           s     sort alphanumerically			  
@@ -94,6 +101,7 @@ a         [de]select all rows       ?     show help buffer
 )
 
 var (
+	// we use our own custom border style
 	customBorder = table.Border{
 		Top:    "─",
 		Left:   "│",
@@ -114,10 +122,17 @@ var (
 		InnerDivider: "│",
 	}
 
-	StyleSelected = lipgloss.NewStyle().Background(lipgloss.Color("#696969")).Foreground(lipgloss.Color("#ffffff"))
-	NoStyle       = lipgloss.NewStyle()
+	// Cells in selected columns will be highlighted
+	StyleSelected = lipgloss.NewStyle().
+			Background(lipgloss.Color("#696969")).
+			Foreground(lipgloss.Color("#ffffff")).
+			Align(lipgloss.Left)
+
+	// the default style
+	NoStyle = lipgloss.NewStyle().Align(lipgloss.Left)
 )
 
+// initializes the table model
 func NewModel(data *Tabdata, ctx *Context) FilterTable {
 	columns := make([]table.Column, len(data.headers))
 	lengths := make([]int, len(data.headers))
@@ -138,15 +153,22 @@ func NewModel(data *Tabdata, ctx *Context) FilterTable {
 		}
 	}
 
-	// setup column data
-	for idx, header := range data.headers {
-		//   FIXME: since  the 3rd  parameter is  not length  but flex
-		// factor, we  get wrong results when resizing. So  we need to
-		// calculate proper flex factors somehow
-		columns[idx] = table.NewFlexColumn(strings.ToLower(header),
-			header, lengths[idx]+2).WithFiltered(true)
+	// determine flexFactor with base 10, used by flexColumns
+	for i, len := range lengths {
+		if len <= 10 {
+			lengths[i] = 1
+		} else {
+			lengths[i] = len / 10
+		}
 	}
 
+	// setup column data with flexColumns
+	for idx, header := range data.headers {
+		columns[idx] = table.NewFlexColumn(strings.ToLower(header),
+			header, lengths[idx]).WithFiltered(true).WithStyle(NoStyle)
+	}
+
+	// separate variable so we can share the row filling code
 	filtertbl := FilterTable{
 		maxColumns: len(data.headers),
 		Rows:       len(data.entries),
@@ -161,6 +183,8 @@ func NewModel(data *Tabdata, ctx *Context) FilterTable {
 	return filtertbl
 }
 
+// Applied to every cell on every change (TAB,up,down key, resize
+// event etc)
 func CellController(input table.StyledCellFuncInput, m FilterTable) lipgloss.Style {
 	if m.headerIdx[input.Column.Key()] == m.ctx.selectedColumn {
 		return StyleSelected
@@ -169,7 +193,8 @@ func CellController(input table.StyledCellFuncInput, m FilterTable) lipgloss.Sty
 	return NoStyle
 }
 
-func (m FilterTable) ToggleSelected() {
+// Selects or deselects ALL rows
+func (m *FilterTable) ToggleAllSelected() {
 	rows := m.Table.GetVisibleRows()
 	selected := m.Table.SelectedRows()
 
@@ -186,6 +211,7 @@ func (m FilterTable) ToggleSelected() {
 	m.Table.WithRows(rows)
 }
 
+// ? pressed, display help message
 func (m FilterTable) ToggleHelp() {
 	m.ctx.showHelp = !m.ctx.showHelp
 }
@@ -194,17 +220,21 @@ func (m FilterTable) Init() tea.Cmd {
 	return nil
 }
 
-// FIXME: possibly re-use it in NewModel() to avoid duplicate code
+// Forward call to context sort
 func (m *FilterTable) Sort(mode string) {
 	m.ctx.Sort(mode)
 	m.fillRows()
 }
 
+// Fills the table rows with our data. Called once on startup and
+// repeatedly if the user changes the sort order in some way
 func (m *FilterTable) fillRows() {
+	// required to be able to feed the model to the controller
 	controllerWrapper := func(input table.StyledCellFuncInput) lipgloss.Style {
 		return CellController(input, *m)
 	}
 
+	// fill the rows with style
 	rows := make([]table.Row, len(m.ctx.data.entries))
 	for idx, entry := range m.ctx.data.entries {
 		rowdata := make(table.RowData, len(entry))
@@ -229,6 +259,7 @@ func (m *FilterTable) fillRows() {
 		Border(customBorder)
 }
 
+// Part of the bubbletea event loop, called every tick
 func (m FilterTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
 		cmd  tea.Cmd
@@ -238,6 +269,8 @@ func (m FilterTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.Table, cmd = m.Table.Update(msg)
 	cmds = append(cmds, cmd)
 
+	// If the user is about to enter filter text, do NOT respond to
+	// key bindings, as they might be part of the filter!
 	if !m.Table.GetIsFilterInputFocused() {
 		switch msg := msg.(type) {
 		case tea.KeyMsg:
@@ -253,7 +286,7 @@ func (m FilterTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				cmds = append(cmds, tea.Quit)
 
 			case "a":
-				m.ToggleSelected()
+				m.ToggleAllSelected()
 
 			case "tab":
 				m.SelectNextColumn()
@@ -277,6 +310,7 @@ func (m FilterTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
+	// Happens when the terminal window has been resized
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
 		m.ctx.totalWidth = msg.Width
@@ -290,6 +324,7 @@ func (m FilterTable) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, tea.Batch(cmds...)
 }
 
+// Add some info to the footer
 func (m *FilterTable) updateFooter() {
 	selected := m.Table.SelectedRows()
 	footer := fmt.Sprintf("selected: %d ", len(selected))
@@ -300,9 +335,10 @@ func (m *FilterTable) updateFooter() {
 		footer = fmt.Sprintf("Filter: %s %s", m.Table.GetCurrentFilter(), footer)
 	}
 
-	m.Table = m.Table.WithStaticFooter(HELP + footer)
+	m.Table = m.Table.WithStaticFooter(HelpFooter + footer)
 }
 
+// Called on resize event (or if help has been toggled)
 func (m *FilterTable) recalculateTable() {
 	m.Table = m.Table.
 		WithTargetWidth(m.calculateWidth()).
@@ -314,6 +350,7 @@ func (m *FilterTable) calculateWidth() int {
 	return m.ctx.totalWidth - m.ctx.horizontalMargin
 }
 
+// Take help height into account, if enabled
 func (m *FilterTable) calculateHeight() int {
 	height := m.Rows + ExtraRows
 
@@ -330,6 +367,7 @@ func (m *FilterTable) calculateHeight() int {
 	return height
 }
 
+// Part of the bubbletable event view, called every tick
 func (m FilterTable) View() string {
 	body := strings.Builder{}
 
@@ -344,6 +382,7 @@ func (m FilterTable) View() string {
 	return body.String()
 }
 
+// User hit the TAB key
 func (m *FilterTable) SelectNextColumn() {
 	if m.ctx.selectedColumn == m.maxColumns-1 {
 		m.ctx.selectedColumn = 0
@@ -352,6 +391,7 @@ func (m *FilterTable) SelectNextColumn() {
 	}
 }
 
+// entry point from outside tablizer into table editor
 func tableEditor(conf *cfg.Config, data *Tabdata) (*Tabdata, error) {
 	// we render to STDERR to avoid dead lock when the user redirects STDOUT
 	// see https://github.com/charmbracelet/bubbletea/issues/860
@@ -361,6 +401,11 @@ func tableEditor(conf *cfg.Config, data *Tabdata) (*Tabdata, error) {
 
 	ctx := &Context{data: data}
 
+	// Output to  STDERR because  there's a  known bubbletea/lipgloss
+	// issue: if  a program with a tui is  expected to write something
+	// to STDOUT when the tui is finished, then the styles do not
+	// work. So we write to STDERR (which works) and tablizer can
+	// still be used inside pipes.
 	program := tea.NewProgram(
 		NewModel(data, ctx),
 		tea.WithOutput(os.Stderr),
@@ -376,6 +421,8 @@ func tableEditor(conf *cfg.Config, data *Tabdata) (*Tabdata, error) {
 		return data, err
 	}
 
+	// Data has been modified. Extract it, put it back into our own
+	// structure and give control back to cmdline tablizer.
 	filteredtable := m.(FilterTable)
 
 	data.entries = make([][]string, len(filteredtable.Table.SelectedRows()))
